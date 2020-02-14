25bb451e1cd4dfdd33eb76dbab5b3e2ffe0623fc
Ken Weaver <kweaver@bricksimple.com>
2020-02-14 15:31:17 -0500

    fix xml problems

--- twilio_demo/call/views.py
+++ twilio_demo/call/views.py
@@ -1,6 +1,6 @@
 from rest_framework.decorators import api_view
 from twilio.twiml.voice_response import VoiceResponse
-from rest_framework.response import Response
+from django.http import HttpResponse
 
 # Create your views here.
 @api_view(['GET', 'POST'])
@@ -8,4 +8,4 @@ def answer_call(request):
     resp = VoiceResponse()
     resp.say('Fuck the law they can eat my dick thats word the pimp')
     print(str(resp))
-    return Response(str(resp), content_type='text/xml')
+    return HttpResponse(str(resp), content_type='text/xml')
