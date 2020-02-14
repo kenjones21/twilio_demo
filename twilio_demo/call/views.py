77586911a0f916fabc603b481cd259815070c82d
Ken Weaver <kweaver@bricksimple.com>
2020-02-14 15:33:42 -0500

    change message

--- twilio_demo/call/views.py
+++ twilio_demo/call/views.py
@@ -6,6 +6,6 @@ from django.http import HttpResponse
 @api_view(['GET', 'POST'])
 def answer_call(request):
     resp = VoiceResponse()
-    resp.say('Fuck the law they can eat my dick thats word the pimp')
+    resp.say('Glad Tom is finally seemingly off calls, I can enjoy the rest of the day in peace.')
     print(str(resp))
     return HttpResponse(str(resp), content_type='text/xml')
