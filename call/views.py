baf136393f14375785ac35b180d0b8246267268c
Ken Weaver <kweaver@bricksimple.com>
2020-02-20 15:21:01 -0500

    jwt token for calls

--- call/views.py
+++ call/views.py
@@ -1,7 +1,7 @@
 from rest_framework.decorators import api_view
 from twilio.twiml.voice_response import VoiceResponse
 from twilio.jwt.client import ClientCapabilityToken
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 from twilio_rest import settings
 
@@ -10,7 +10,6 @@ from twilio_rest import settings
 def answer_call(request):
     resp = VoiceResponse()
     resp.say('Glad Tom is finally seemingly off calls, I can enjoy the rest of the day in peace.')
-    print(str(resp))
     return HttpResponse(str(resp), content_type='text/xml')
 
 def get_token(request):
@@ -18,3 +17,7 @@ def get_token(request):
         settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
 
     capability.allow_client_outgoing(settings.TWIML_APPLICATION_SID)
+    capability.allow_client_incoming('general')
+
+    token = capability.to_jwt()
+    return JsonResponse({'token': token.decode('utf-8')})
